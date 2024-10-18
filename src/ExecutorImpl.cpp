7a59f3115cdc5bfd38fc3b0c81d3edabe6df40ce
Wild-Donkey <fysh4202@163.com>
2024-10-18 16:03:25 +0800

    NewTest

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -3,9 +3,10 @@
 #include <new>
 
 namespace adas {
-ExecutorImpl::ExecutorImpl(const Pose &pose) noexcept {}
+ExecutorImpl::ExecutorImpl(const Pose &pose) noexcept : pose(pose) {}
 Pose ExecutorImpl::Query(void) const noexcept { return pose; }
 Executor *Executor::NewExecutor(const Pose &pose) noexcept {
   return new (std::nothrow) ExecutorImpl(pose);
 }
+void ExecutorImpl::Execute(const std::string &commands) noexcept {}
 }  // namespace adas
--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -13,7 +13,6 @@ bool operator==(const Pose &lhs, const Pose &rhs) {
 }
 
 TEST(ExecutorTest, should_return_init_pose_when_without_command) {
-  printf("114514");
   std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
   const Pose target{0, 0, 'E'};
   ASSERT_EQ(target, executor->Query());
@@ -22,6 +21,12 @@ TEST(ExecutorTest, should_return_init_pose_when_without_command) {
 TEST(ExecutorTest, should_return_default_pose_when_without_init_and_command) {
   std::unique_ptr<Executor> executor(Executor::NewExecutor());
   const Pose target{0, 0, 'N'};
+  ASSERT_EQ(target, executor->Query());
+}
+TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E) {
+  std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+  executor->Execute("M");
+  const Pose target({1, 0, 'E'});
   ASSERT_EQ(target, executor->Query());
 }
 
