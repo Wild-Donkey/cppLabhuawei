5fae8a4c8fa9e6e5bf2f8e0153f0b84f4f0b4f43
Wild-Donkey <fysh4202@163.com>
2024-10-18 16:11:50 +0800

    PassedTest3

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -1,5 +1,6 @@
 #include "ExecutorImpl.hpp"
 
+#include <map>
 #include <new>
 
 namespace adas {
@@ -8,5 +9,22 @@ Pose ExecutorImpl::Query(void) const noexcept { return pose; }
 Executor *Executor::NewExecutor(const Pose &pose) noexcept {
   return new (std::nothrow) ExecutorImpl(pose);
 }
-void ExecutorImpl::Execute(const std::string &commands) noexcept {}
+void ExecutorImpl::Execute(const std::string &commands) noexcept {
+  std::map<char, std::pair<int, int> > Dire;
+  std::map<char, char> Lef, Rig;
+  Dire['E'] = {1, 0};
+  Dire['W'] = {-1, 0};
+  Dire['S'] = {0, -1};
+  Dire['N'] = {0, 1};
+  Rig[Lef['E'] = 'N'] = 'E';
+  Rig[Lef['W'] = 'S'] = 'W';
+  Rig[Lef['S'] = 'E'] = 'S';
+  Rig[Lef['N'] = 'W'] = 'N';
+  for (auto i : commands) {
+    if (i == 'M') {
+      pose.x += Dire[pose.heading].first;
+      pose.y += Dire[pose.heading].second;
+    }
+  }
+}
 }  // namespace adas
