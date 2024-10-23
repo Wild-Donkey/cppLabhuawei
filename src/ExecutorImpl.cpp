bef8fb07690b6945c47a5379cc8c8b5e909d89f5
Micheal <fysh4202@163.com>
2024-10-23 21:05:45 +0800

    fix: bugs of extract

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -4,31 +4,6 @@
 #include <new>
 
 namespace adas {
-  void ExecutorImpl::Move(void) {
-    if (pose.heading == 'E' || pose.heading == 'W')
-      pose.x += (pose.heading == 'E' ? 1 : -1);
-    else
-      pose.y += (pose.heading == 'N' ? 1 : -1);
-  }
-  void ExecutorImpl::TurnLeft(void) {
-    if (pose.heading == 'E') pose.heading = 'N';
-    else if (pose.heading == 'W') pose.heading = 'S';
-    else if (pose.heading == 'S') pose.heading = 'E';
-    else pose.heading = 'W';
-  }
-  void ExecutorImpl::TurnRight(void) {
-    if (pose.heading == 'N') pose.heading = 'E';
-    else if (pose.heading == 'S') pose.heading = 'W';
-    else if (pose.heading == 'E') pose.heading = 'S';
-    else pose.heading = 'N';
-  }
-  ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept
-    : pose(pose), isFast(false) {
-  }
-  Pose ExecutorImpl::Query(void) const noexcept { return pose; }
-  Executor* Executor::NewExecutor(const Pose& pose) noexcept {
-    return new (std::nothrow) ExecutorImpl(pose);
-  }
   void ExecutorImpl::Execute(const std::string& commands) noexcept {
     for (auto i : commands) {
       if (i == 'F') {
@@ -40,5 +15,30 @@ namespace adas {
       if (i == 'L') TurnLeft();
       if (i == 'R') TurnRight();
     }
+  }
+  void ExecutorImpl::Move() noexcept {
+    if (pose.heading == 'E' || pose.heading == 'W')
+      pose.x += (pose.heading == 'E' ? 1 : -1);
+    else
+      pose.y += (pose.heading == 'N' ? 1 : -1);
+  }
+  void ExecutorImpl::TurnLeft() noexcept {
+    if (pose.heading == 'E') pose.heading = 'N';
+    else if (pose.heading == 'W') pose.heading = 'S';
+    else if (pose.heading == 'S') pose.heading = 'E';
+    else pose.heading = 'W';
+  }
+  void ExecutorImpl::TurnRight() noexcept {
+    if (pose.heading == 'N') pose.heading = 'E';
+    else if (pose.heading == 'S') pose.heading = 'W';
+    else if (pose.heading == 'E') pose.heading = 'S';
+    else pose.heading = 'N';
+  }
+  ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept
+    : pose(pose), isFast(false) {
+  }
+  Pose ExecutorImpl::Query(void) const noexcept { return pose; }
+  Executor* Executor::NewExecutor(const Pose& pose) noexcept {
+    return new (std::nothrow) ExecutorImpl(pose);
   }
 }  // namespace adas
