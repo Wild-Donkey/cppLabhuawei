1dee54a4a2eb0fb72b80dfb9f8c7876118f7043d
Wild-Donkey <fysh4202@163.com>
2024-11-08 19:26:17 +0800

    command to extract independent file

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -1,47 +1,58 @@
 #include "ExecutorImpl.hpp"
 
 #include <map>
-#include <new>
 #include <memory>
+#include <new>
+
+#include "Command.hpp"
 
 namespace adas {
-  void ExecutorImpl::Execute(const std::string& commands) noexcept {
-    for (auto i : commands) {
-      if (i == 'F') {
-        isFast ^= 1;
-        continue;
-      }
-      std::unique_ptr<ICommand> cmder;
-      cmder = std::make_unique<MoveCommand>();
-      if (isFast) cmder->DoOperate(*this);
-      if (i == 'L') cmder = std::make_unique<TurnLeftCommand>();
-      if (i == 'R') cmder = std::make_unique<TurnRightCommand>();
-      cmder->DoOperate(*this);
+void ExecutorImpl::Fast(void) noexcept {}
+bool ExecutorImpl::IsFast(void) const noexcept { return isFast; }
+void ExecutorImpl::Execute(const std::string& commands) noexcept {
+  for (auto i : commands) {
+    if (i == 'F') {
+      isFast ^= 1;
+      continue;
     }
+    std::unique_ptr<ICommand> cmder;
+    cmder = std::make_unique<MoveCommand>();
+    if (isFast) cmder->DoOperate(*this);
+    if (i == 'L') cmder = std::make_unique<TurnLeftCommand>();
+    if (i == 'R') cmder = std::make_unique<TurnRightCommand>();
+    cmder->DoOperate(*this);
   }
-  void ExecutorImpl::Move() noexcept {
-    if (pose.heading == 'E' || pose.heading == 'W')
-      pose.x += (pose.heading == 'E' ? 1 : -1);
-    else
-      pose.y += (pose.heading == 'N' ? 1 : -1);
-  }
-  void ExecutorImpl::TurnLeft() noexcept {
-    if (pose.heading == 'E') pose.heading = 'N';
-    else if (pose.heading == 'W') pose.heading = 'S';
-    else if (pose.heading == 'S') pose.heading = 'E';
-    else pose.heading = 'W';
-  }
-  void ExecutorImpl::TurnRight() noexcept {
-    if (pose.heading == 'N') pose.heading = 'E';
-    else if (pose.heading == 'S') pose.heading = 'W';
-    else if (pose.heading == 'E') pose.heading = 'S';
-    else pose.heading = 'N';
-  }
-  ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept
-    : pose(pose), isFast(false) {
-  }
-  Pose ExecutorImpl::Query(void) const noexcept { return pose; }
-  Executor* Executor::NewExecutor(const Pose& pose) noexcept {
-    return new (std::nothrow) ExecutorImpl(pose);
-  }
+}
+void ExecutorImpl::Move() noexcept {
+  if (pose.heading == 'E' || pose.heading == 'W')
+    pose.x += (pose.heading == 'E' ? 1 : -1);
+  else
+    pose.y += (pose.heading == 'N' ? 1 : -1);
+}
+void ExecutorImpl::TurnLeft() noexcept {
+  if (pose.heading == 'E')
+    pose.heading = 'N';
+  else if (pose.heading == 'W')
+    pose.heading = 'S';
+  else if (pose.heading == 'S')
+    pose.heading = 'E';
+  else
+    pose.heading = 'W';
+}
+void ExecutorImpl::TurnRight() noexcept {
+  if (pose.heading == 'N')
+    pose.heading = 'E';
+  else if (pose.heading == 'S')
+    pose.heading = 'W';
+  else if (pose.heading == 'E')
+    pose.heading = 'S';
+  else
+    pose.heading = 'N';
+}
+ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept
+    : pose(pose), isFast(false) {}
+Pose ExecutorImpl::Query(void) const noexcept { return pose; }
+Executor* Executor::NewExecutor(const Pose& pose) noexcept {
+  return new (std::nothrow) ExecutorImpl(pose);
+}
 }  // namespace adas
--- src/ExecutorImpl.hpp
+++ src/ExecutorImpl.hpp
@@ -5,48 +5,35 @@
 
 namespace adas {
 
-  class ExecutorImpl final : public Executor {
-  public:
-    // 构造函数
-    explicit ExecutorImpl(const Pose& pose) noexcept;
+class ExecutorImpl final : public Executor {
+ public:
+  // 构造函数
+  explicit ExecutorImpl(const Pose& pose) noexcept;
 
-    // 默认析构函数
-    ~ExecutorImpl() noexcept = default;
+  // 默认析构函数
+  ~ExecutorImpl() noexcept = default;
 
-    // 不能拷贝
-    ExecutorImpl(const ExecutorImpl&) = delete;
+  // 不能拷贝
+  ExecutorImpl(const ExecutorImpl&) = delete;
 
-    // 不能赋值
-    ExecutorImpl& operator=(const ExecutorImpl&) = delete;
+  // 不能赋值
+  ExecutorImpl& operator=(const ExecutorImpl&) = delete;
 
-  public:
-    // 查询当前汽车姿态，是父类抽象方法Query的具体实现
-    Pose Query(void) const noexcept override;
-    void Execute(const std::string& commands) noexcept override;
+ public:
+  // 查询当前汽车姿态，是父类抽象方法Query的具体实现
+  Pose Query(void) const noexcept override;
+  void Execute(const std::string& commands) noexcept override;
 
-  private:
-    class ICommand {
-    public:
-      virtual ~ICommand() {}
-      virtual void DoOperate(ExecutorImpl& executor)const = 0;
-    };
-    class MoveCommand final : public ICommand {
-    public:
-      void DoOperate(ExecutorImpl& executor) const noexcept override { executor.Move(); }
-    };
-    class TurnLeftCommand final : public ICommand {
-    public:
-      void DoOperate(ExecutorImpl& executor) const noexcept override { executor.TurnLeft(); }
-    };
-    class TurnRightCommand final : public ICommand {
-    public:
-      void DoOperate(ExecutorImpl& executor) const noexcept override { executor.TurnRight(); }
-    };
-    void Move(void) noexcept;
-    void TurnLeft(void) noexcept;
-    void TurnRight(void) noexcept;
-    Pose pose;
-    bool isFast;
-  };
+ public:
+  void Move(void) noexcept;
+  void TurnLeft(void) noexcept;
+  void TurnRight(void) noexcept;
+  void Fast(void) noexcept;
+  bool IsFast(void) const noexcept;
+
+ private:
+  Pose pose;
+  bool isFast;
+};
 
 }  // namespace adas
