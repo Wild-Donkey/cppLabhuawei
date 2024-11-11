df5b16203c81df7cd96854d438c01b40c5f48fe6
Wild-Donkey <fysh4202@163.com>
2024-11-11 19:11:04 +0800

    add ExecutorReverseTest

--- tests/ExecutorFastTest.cpp
+++ tests/ExecutorFastTest.cpp
@@ -6,7 +6,7 @@
 #include "PoseEq.hpp"
 
 namespace adas {
-TEST(ExecutorTest,
+TEST(ExecutorFastTest,
      should_return_x_plus_2_given_status_is_fast_command_is_M_and_facing_is_E) {
   std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
   executor->Execute("FM");
@@ -14,7 +14,7 @@ TEST(ExecutorTest,
   ASSERT_EQ(target, executor->Query());
 }
 TEST(
-    ExecutorTest,
+    ExecutorFastTest,
     should_return_N_and_x_plus_1_given_status_is_fast_command_is_L_and_facing_is_E) {
   std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
   executor->Execute("FL");
@@ -22,14 +22,14 @@ TEST(
   ASSERT_EQ(target, executor->Query());
 }
 TEST(
-    ExecutorTest,
+    ExecutorFastTest,
     should_return_S_and_x_plus_1_given_status_is_fast_given_command_is_R_and_facing_is_E) {
   std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
   executor->Execute("FR");
   const Pose target({1, 0, 'S'});
   ASSERT_EQ(target, executor->Query());
 }
-TEST(ExecutorTest,
+TEST(ExecutorFastTest,
      should_return_y_plus_1_given_command_is_FFM_and_facing_is_N) {
   std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
   executor->Execute("FFM");
