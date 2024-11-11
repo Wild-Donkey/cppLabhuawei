62747f9e454c41e8716f3524643333cd3e78a816
Wild-Donkey <fysh4202@163.com>
2024-11-11 19:13:18 +0800

    add ExecutorReverseFastTest

--- tests/ExecutorReverseFastTest.cpp
+++ tests/ExecutorReverseFastTest.cpp
@@ -4,11 +4,10 @@
 #include "PoseEq.hpp"
 
 namespace adas {
-/*
 TEST(
     ExecutorReverseFastTest,
-    should_return_x_minus_2_given_status_is_fast_and_reverse_command_is_M_and_facing_is_E)
-{ std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    should_return_x_minus_2_given_status_is_fast_and_reverse_command_is_M_and_facing_is_E) {
+  std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
   executor->Execute("FBM");
   const Pose target{-2, 0, 'E'};
   ASSERT_EQ(target, executor->Query());
@@ -16,8 +15,8 @@ TEST(
 
 TEST(
     ExecutorReverseFastTest,
-    should_return_S_and_x_minus_1_given_status_is_fast_command_is_L_and_facing_is_E)
-{ std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    should_return_S_and_x_minus_1_given_status_is_fast_command_is_L_and_facing_is_E) {
+  std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
   executor->Execute("FBL");
   const Pose target{-1, 0, 'S'};
   ASSERT_EQ(target, executor->Query());
@@ -25,83 +24,10 @@ TEST(
 
 TEST(
     ExecutorReverseFastTest,
-    should_return_N_and_x_minus_1_given_is_fast_and_reverse_given_command_is_R_and_facing_is_E)
-{ std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    should_return_N_and_x_minus_1_given_is_fast_and_reverse_given_command_is_R_and_facing_is_E) {
+  std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
   executor->Execute("FBR");
   const Pose target{-1, 0, 'N'};
   ASSERT_EQ(target, executor->Query());
 }
-TEST(
-    ExecutorReverseFastTest,
-    should_return_N_and_x_minus_1_given_is_fast_and_reverse_given_command_is_R_and_facing_is_E)
-{ std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
-  executor->Execute("BBM");
-  const Pose target{-1, 0, 'N'};
-  ASSERT_EQ(target, executor->Query());
-}
-TEST(
-    ExecutorReverseFastTest,
-    should_return_N_and_x_minus_1_given_is_fast_and_reverse_given_command_is_R_and_facing_is_E)
-{ std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
-  executor->Execute("BBL");
-  const Pose target{-1, 0, 'N'};
-  ASSERT_EQ(target, executor->Query());
-}
-TEST(
-    ExecutorReverseFastTest,
-    should_return_N_and_x_minus_1_given_is_fast_and_reverse_given_command_is_R_and_facing_is_E)
-{ std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
-  executor->Execute("BBR");
-  const Pose target{-1, 0, 'N'};
-  ASSERT_EQ(target, executor->Query());
-}
-TEST(
-    ExecutorReverseFastTest,
-    should_return_N_and_x_minus_1_given_is_fast_and_reverse_given_command_is_R_and_facing_is_E)
-{ std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
-  executor->Execute("FBR");
-  const Pose target{-1, 0, 'N'};
-  ASSERT_EQ(target, executor->Query());
-}
-TEST(
-    ExecutorReverseFastTest,
-    should_return_N_and_x_minus_1_given_is_fast_and_reverse_given_command_is_R_and_facing_is_E)
-{ std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
-  executor->Execute("FBR");
-  const Pose target{-1, 0, 'N'};
-  ASSERT_EQ(target, executor->Query());
-}
-TEST(
-    ExecutorReverseFastTest,
-    should_return_N_and_x_minus_1_given_is_fast_and_reverse_given_command_is_R_and_facing_is_E)
-{ std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
-  executor->Execute("FBR");
-  const Pose target{-1, 0, 'N'};
-  ASSERT_EQ(target, executor->Query());
-}
-TEST(
-    ExecutorReverseFastTest,
-    should_return_N_and_x_minus_1_given_is_fast_and_reverse_given_command_is_R_and_facing_is_E)
-{ std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
-  executor->Execute("FBR");
-  const Pose target{-1, 0, 'N'};
-  ASSERT_EQ(target, executor->Query());
-}
-TEST(
-    ExecutorReverseFastTest,
-    should_return_N_and_x_minus_1_given_is_fast_and_reverse_given_command_is_R_and_facing_is_E)
-{ std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
-  executor->Execute("FBR");
-  const Pose target{-1, 0, 'N'};
-  ASSERT_EQ(target, executor->Query());
-}
-TEST(
-    ExecutorReverseFastTest,
-    should_return_N_and_x_minus_1_given_is_fast_and_reverse_given_command_is_R_and_facing_is_E)
-{ std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
-  executor->Execute("FBR");
-  const Pose target{-1, 0, 'N'};
-  ASSERT_EQ(target, executor->Query());
-}
-*/
 }  // namespace adas
